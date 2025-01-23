b84c5818011bbdcc97706643fdfcdec6fd4827b3
Randy Le <le.hy.randy@gmail.com>
2025-01-22 20:49:18 -0600

    adding completed question 2 html,js file from beginner js practice

---
+++ question2.js
@@ -0,0 +1,8 @@
+const number1 = prompt("Please enter your first number")
+const number2 = prompt("Please enter your second number")
+const number3 = prompt("Please enter your third number")
+
+const largest = Math.max(number1, number2, number3)
+
+
+alert(`The largest number was: ${largest}`)
