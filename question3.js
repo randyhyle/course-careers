fc552c31c3a49233b732f9fc55190e1e25b69a9c
Randy Le <le.hy.randy@gmail.com>
2025-01-23 20:34:13 -0600

    fixed a small bug, missing semicolon and added comments

--- question3.js
+++ question3.js
@@ -1,3 +1,7 @@
+/*
+Min and max can be interchanged with this program
+as long as min is less than max
+*/
 
 const min = -3
 const max = -9
@@ -5,7 +9,7 @@ const max = -9
 let arr = []
 
 for (let i = min; i <= max; i ++) {
-    arr.push(i)
+    arr.push(i);
 }
 
 console.log(arr)
