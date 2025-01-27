4bd7ab583331ccccaac90597b0a30d01f7862e0c
Randy Le <le.hy.randy@gmail.com>
2025-01-26 20:55:20 -0600

    added comments for question5

--- question5.js
+++ question5.js
@@ -1,4 +1,8 @@
 function mathIsFun(numberString) {
+    /*
+    This is the more efficient approach to question 5.
+    Runs in O(n) constant time and O(1) space complexity theoretically
+    */
     const numbers = numberString.split("|")
     let largest = Math.max(Number(numbers[0]), Number(numbers[1]))
     let second = Math.min(Number(numbers[0]), Number(numbers[1]))
