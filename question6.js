e75f57ccf74464d5cea95b0efabd751b9139ed7b
Randy Le <le.hy.randy@gmail.com>
2025-01-28 23:27:45 -0600

    implementation of question6.js function

--- question6.js
+++ question6.js
@@ -7,4 +7,33 @@ function friends(people) {
     Count loyal friends for each person.
     Find the person with the highest count and return their name.
     */
+
+    loyal = 0
+    most = ""
+
+    for (let person of people) {
+        let count = 0
+        for (let friend of person.friends) {
+            let friendObj = people.find(p => p.name === friend);
+
+            if (friendObj && friendObj.friends.includes(person.name)) {
+                count++;
+            }
+
+        }
+        if (count > loyal) {
+            loyal = count;
+            most = person.name;
+        }
+    }
+    return most
 }
+
+people = [
+    {"name": "Tim", "friends": ["John", "Sally"]},
+    {"name": "John", "friends": ["Tim", "Mike"]},
+    {"name": "Mike", "friends": []},
+    {"name": "Sally", "friends": ["Tim"]}
+]
+
+console.log(friends(people))
