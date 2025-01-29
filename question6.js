eba06bec367ee24d3291fca99211a27cd72616d9
Randy Le <le.hy.randy@gmail.com>
2025-01-28 23:02:18 -0600

    added comment for intuition for question6.js

---
+++ question6.js
@@ -0,0 +1,10 @@
+function friends(people) {
+    /*
+    Loop through each person and check their "friends" list.
+    For each friend in the list, check if the friendship is mutual:
+    Look up the friend's object in people.
+    Check if the current person's name is in the friend's "friends" list.
+    Count loyal friends for each person.
+    Find the person with the highest count and return their name.
+    */
+}
