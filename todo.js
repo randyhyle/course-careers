2885ab06b40ad56a898888dc3d982ba3da658610
Randy Le <le.hy.randy@gmail.com>
2025-02-10 21:10:50 -0600

    added onclick remove function

--- todo.js
+++ todo.js
@@ -1,12 +1,17 @@
 function addTodo() {
-    const todo = document.getElementById("new-todo").value;
+    const todoElement = document.getElementById("new-todo");
+    const todo = todoElement.value;
+    todoElement.value = "";
+
     if (todo.length < 3) {
         return alert("You must have at least 3 characters for your TODO")
     }
     const list = document.getElementById("list");
     const li = document.createElement("li");
     const text = document.createTextNode(todo);
+    
+    li.setAttribute("onclick", "this.remove()")
+    
     li.appendChild(text);
-
     list.appendChild(li)
 }
